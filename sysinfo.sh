cdae9357e67734c2cce11361d0032c86633c1f13
Shaun Rodrigues <shaun.rodrigues@globalpayex.com>
2025-07-10 17:24:02 +0530

    Fixed decor

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "System Information:" 
-echo "-----------------------"
+echo "*---------------------*"
 echo "Current User: $(whoami)"
 echo "Machine Name: $(hostname)"
 echo "Operating System: $(uname -s)"
@@ -10,3 +10,4 @@ echo "RAM specifics: $(free -h | grep '^Mem:')"
 echo "Total RAM: $(free -h | grep '^Mem:' | awk '{print $2}')GB"
 echo "Used RAM: $(free -h | grep '^Mem:' | awk '{print $3}')GB"
 echo "Available RAM: $(free -h | grep '^Mem:' | awk '{print $4}')GB"
+echo "*---------------------*"
