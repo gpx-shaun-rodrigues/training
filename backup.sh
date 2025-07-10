4f9b1d505ff5aa7aec4a2c0b11f028a33f9b3311
Shaun Rodrigues <shaun.rodrigues@globalpayex.com>
2025-07-10 15:13:55 +0530

    Linux crash course completed

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Script for Backing up"
+SOURCE="$1"
+BACKUP_DIR="$2"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+mkdir -p "$BACKUP_DIR"
+if [ -d "$SOURCE" ]; then
+    echo "Backing up directory: $SOURCE"
+    tar -czf "$BACKUP_FILE" -C "$SOURCE" .
+else
+    echo "Source is not a valid directory: $SOURCE"
+    exit 1
+fi
---
+++ filecount.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+EXTENSION="$1"
+DIRECTORY="$2"
+
+WILDCARD_PATH="$DIRECTORY/*.$EXTENSION"
+COUNT=0
+
+for file in $WILDCARD_PATH; do
+    if [ -f "$file" ]; then
+        COUNT=$((COUNT+1))
+    fi
+done
+
+echo "Number of files with extension .$EXTENSION in  $DIRECTORY: $COUNT"
